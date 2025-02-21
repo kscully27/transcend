7b463b0161d50ba86b36b0bfdf830f151a3a25f8
Kevin Scully <kscully27@gmail.com>
2025-02-21 13:01:55 -0600

    test: tests commit hooks

--- lib/src/ui/clay/clay_container.dart
+++ lib/src/ui/clay/clay_container.dart
@@ -150,7 +150,7 @@ class _ClayContainerState extends State<ClayContainer> {
           end: Alignment.bottomRight,
           colors: gradientColors,
         ),
-        boxShadow: shadowList,
+        boxShadow: shadowList2,
       ),
       child: widget.child,
     );
